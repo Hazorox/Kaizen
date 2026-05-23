53b4b1ff365e841f2018c0d536d013d0f7dc7df6
Hamza Awad <hamzaawad8899@gmail.com>
2026-05-23 23:25:38 +0300

    login/register page design done

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,6 +1,5 @@
 import { LuPickaxe, LuSwords } from "react-icons/lu";
 import {
-  MdKey,
   MdLocalFireDepartment,
   MdMail,
   MdOutlineLocalFireDepartment,
@@ -10,8 +9,14 @@ import Card from "../components/Card";
 import { IoDiamondOutline } from "react-icons/io5";
 import { RiSwordFill } from "react-icons/ri";
 import WordRow from "../components/WordRow";
-import { FaArrowRight, FaEyeSlash, FaRegEye } from "react-icons/fa";
-import { useRef, useState } from "react";
+import {
+  FaArrowRight,
+  FaEyeSlash,
+  FaRegEye,
+  FaShieldAlt,
+  FaUserCircle,
+} from "react-icons/fa";
+import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 
 const Homepage = () => {
@@ -19,7 +24,9 @@ const Homepage = () => {
   const time = new Date().getHours();
   const streak = 3;
   const username = "Hazoro";
-  const [login, setLogin] = useState(false);
+  const [login, setLogin] = useState(true);
+  const [repeatedPass, setRepeatedPassInput] = useState("");
+  const [showRepeated, setShowRepeated] = useState(false);
   const [show, setShow] = useState(true);
   const [passInput, setPassInput] = useState("");
   const bgs = login ? ["bg-[#FF9A3C]", ""] : ["", "bg-[#FF9A3C]"];
@@ -205,24 +212,24 @@ const Homepage = () => {
           <div className="p-12 w-1/2 h-full flex justify-center items-center">
             <div className="bg-[#fffbe6] relatve flex flex-col justify-around items-center w-full border-8 h-full rounded-xl">
               <div className="w-full flex justify-center flex-col items-center gap-8">
-              <div className="flex border-2 rounded-lg w-[70%]">
-                <span
-                  className={
-                    bgs[0] +
-                    " flex justify-center items-center w-1/2 rounded-l-md py-2"
-                  }
-                >
-                  Login
-                </span>
-                <span
-                  className={
-                    bgs[1] +
-                    " flex justify-center items-center w-1/2 rounded-r-md"
-                  }
-                >
-                  Register
-                </span>
-              </div>
+                <div className="flex border-2 rounded-lg w-[70%]">
+                  <span
+                    className={
+                      bgs[0] +
+                      " flex justify-center items-center w-1/2 rounded-l-md py-2"
+                    }
+                  >
+                    Login
+                  </span>
+                  <span
+                    className={
+                      bgs[1] +
+                      " flex justify-center items-center w-1/2 rounded-r-md"
+                    }
+                  >
+                    Register
+                  </span>
+                </div>
                 <div className="relative w-[70%] justify-center items-center">
                   <MdMail
                     size={20}
@@ -234,6 +241,20 @@ const Homepage = () => {
                     placeholder="E-Mail Address"
                   />
                 </div>
+
+                {!login ? (
+                  <div className="relative w-[70%] justify-center items-center">
+                    <FaUserCircle
+                      size={20}
+                      className="absolute left-3 top-1/2 -translate-y-1/2 opacity-80"
+                    />
+                    <input
+                      type="text"
+                      className="px-2 pl-10 w-full border-2 h-12 rounded-md"
+                      placeholder="Username"
+                    />
+                  </div>
+                ) : null}
                 <div className="relative w-[70%] justify-center items-center">
                   <MdVpnKey
                     size={20}
@@ -269,8 +290,64 @@ const Homepage = () => {
                     ""
                   )}
                 </div>
-                  <button className="bg-[#FF9A3C] p-4 w-[70%] rounded-full text-xl -mb-4">{login? "Login":"Register"}</button>
-                  <button className="bg-[#FF9A3C] p-2 w-[70%] rounded-full flex justify-center text-xl items-center">Continue With <FcGoogle className="inline ml-3" size={48}/> </button>
+                {!login ? (
+                  <div className="relative w-[70%] justify-center items-center">
+                    <FaShieldAlt
+                      size={20}
+                      className="absolute left-3 top-1/2 -translate-y-1/2 opacity-80"
+                    />
+                    <input
+                      type={showRepeated ? "text" : "password"}
+                      value={repeatedPass}
+                      onChange={(e) => setRepeatedPassInput(e.target.value)}
+                      className="px-2 pl-10 w-full border-2 h-12 rounded-md"
+                      placeholder="Repeat Password"
+                      id="repeatedPass"
+                    />
+
+                    {repeatedPass.length > 0 ? (
+                      show ? (
+                        <FaEyeSlash
+                          onClick={() => {
+                            setShowRepeated((prev) => !prev);
+                          }}
+                          size={20}
+                          className="cursor-pointer absolute right-3 top-1/2 -translate-y-1/2"
+                        />
+                      ) : (
+                        <FaRegEye
+                          size={20}
+                          className="cursor-pointer absolute right-3 top-1/2 -translate-y-1/2"
+                          onClick={() => {
+                            setShowRepeated((prev) => !prev);
+                          }}
+                        />
+                      )
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                ) : null}
+                {login ? (
+                  <>
+                    <button className="bg-[#FF9A3C] p-4 w-[70%] rounded-full text-xl -mb-4">
+                      {login ? "Login" : "Register"}
+                    </button>
+                    <button className="bg-[#FF9A3C] p-2 w-[70%] rounded-full flex justify-center text-xl items-center">
+                      Continue With{" "}
+                      <FcGoogle className="inline ml-3" size={48} />{" "}
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <div className="flex w-[70%] rounded-xl border justify-around items-center bg-[#FF9A3C]">
+                      <button className="border-r-2 h-full text-xl w-1/2">Register</button>
+                      <button className="w-1/2">
+                        <FcGoogle className="inline" size={48} />
+                      </button>
+                    </div>
+                  </>
+                )}
               </div>
             </div>
           </div>
