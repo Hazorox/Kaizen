ebd4cbbd25c4869c3302bc3b467061acbb9a9de6
Hamza Awad <hamzaawad8899@gmail.com>
2026-06-05 20:09:29 +0300

    smth to check later on just to assure secret exists

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -7,6 +7,7 @@ export const authMiddleware = (
 ) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ error: "No Token" });
+  if(!process.env.JWT_SECRET) console.log("JWT SECRET NOT FOUND")
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET ?? "secret");
     (req as any).user = decoded;
