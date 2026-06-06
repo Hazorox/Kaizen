e333c5916455033b304066ceae7f749cf1ccf67e
Hamza Awad <hamzaawad8899@gmail.com>
2026-06-07 01:57:14 +0300

    supressed error messages to manage them myself

--- client/src/api/baseAPI.ts
+++ client/src/api/baseAPI.ts
@@ -1,11 +1,17 @@
 import axios from "axios";
 import { getToken } from "../utils/token";
 const API = axios.create({ baseURL: "http://localhost:9898/api" });
-API.interceptors.request.use((config) => {
-  const token = getToken();
-  if (token) {
-    config.headers.set("Authorization", `Bearer ${token}`);
-  }
-  return config;
-});
+API.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    if (token) config.headers.set("Authorization", `Bearer ${token}`);
+    return config;
+  },
+  (error) => Promise.resolve(error),
+);
+
+API.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.resolve(error.response),
+);
 export default API;
