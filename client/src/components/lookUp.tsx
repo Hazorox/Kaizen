ee19e50d93840a63b6a474727107290dc7e19444
Hamza Awad <hamzaawad8899@gmail.com>
2026-06-06 04:04:07 +0300

    added Anki failed Toast

--- client/src/components/lookUp.tsx
+++ client/src/components/lookUp.tsx
@@ -6,10 +6,15 @@ import { PiSpeakerHighBold } from "react-icons/pi";
 import { Tooltip } from "react-tooltip";
 import { colors } from "../constants";
 import { ankiAddCard } from "../api/anki";
+import toast, { Toaster } from "react-hot-toast"
 const LookUp = ({ text, sub = true }: { sub?: boolean; text: string }) => {
   // Defining the type of entries to make a type of list of them would be long and not really important, so ditched it
   const [data, setData] = useState(null);
   const [audio, setAudio] = useState("");
+  const failedAddCard = ()=>{toast.error("Failed. Check if Anki is open or the card already exists.",{icon:<img src="/anki.svg" />,style:{
+    background:"#e87d81",
+    color:"#fffbe6"
+  }})}
   useEffect(() => {
     async function fetchStuff() {
       if (!text) return;
@@ -24,6 +29,7 @@ const LookUp = ({ text, sub = true }: { sub?: boolean; text: string }) => {
   }, [text]);
   return (
     <AnimatePresence>
+      <Toaster position="top-center" reverseOrder={false} toastOptions={{duration:2500,removeDelay:750}} />
       {data && (
         <motion.div
           exit={{ height: 0 }}
@@ -96,8 +102,8 @@ const LookUp = ({ text, sub = true }: { sub?: boolean; text: string }) => {
                     <motion.button
                       onClick={async () => {
                         const res = await ankiAddCard(front, back);
-                        if (!res) console.log("FAILED");
-                        if (res) console.log("WORKED BOI",res);
+                        if (!res) failedAddCard();
+                        if (res) toast.success("Successfully Added the Card !");
                       }}
                       whileHover={{ scale: 1.1 }}
                       transition={{ duration: 0.15 }}
