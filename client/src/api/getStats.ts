0432f5f2bdc2f693615885d00c60f066f6da3bd3
Hamza Awad <hamzaawad8899@gmail.com>
2026-06-08 10:49:11 +0300

    some modifications idk

--- client/src/api/getStats.ts
+++ client/src/api/getStats.ts
@@ -1,12 +1,12 @@
 import API from "./baseAPI"
 
-export const getMined = async ()=>{
-    const res = await API.get("/immersion/today")
+export const getStats =async()=>{
+    const res = await API.get("/getStats")
     if(res.data.error) return;
     return res.data
 }
-export const getMatches = async ()=>{
-    const res = await API.get("/battle/numToday")
-    if (res.data.error) return;
+export const totalMined = async ()=>{
+    const res = await API.get("/immersion/total")
+    if(res.data.error) return;
     return res.data
 }
--- server/src/utils/getStats.ts
+++ server/src/utils/getStats.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth";
 import { UserStat } from "../models/UserStat";
+import { error } from "console";
 const router = Router();
 const bothSameDay = (a: Date, b: Date) =>
   a.getMonth() === b.getMonth() &&
@@ -17,5 +18,12 @@ router.get("/api/getStats", async (req, res) => {
   const matchesToday = response.battleHistory.filter((entry:any)=>bothSameDay(today,entry.time)).length
   res.json({mining:minedToday,matches:matchesToday})
 });
+
+router.get("/api/immersion/total",async (req,res)=>{
+  const username = (req as any).user.username
+  const response = await UserStat.findOne({username})
+  if(!response) return res.json({error:"404"})
+  res.json(response.mining.length)
+})
 
 export const getStats = router;
