8e38d10193885740d71ccd0ed806be4e800a0f62
Hamza Awad <hamzaawad8899@gmail.com>
2026-06-25 07:47:40 +0300

    try to use id instead of url

--- client/src/api/ytSub.ts
+++ client/src/api/ytSub.ts
@@ -1,9 +1,7 @@
 import API from "./baseAPI";
 export const getSub = async (input: string) => {
-  
-    const res = await API.post(`/transcript`,{input});
-    if(res.data.error=="404") return []
-    if(res.data.error=="invalid") return "invalid"
-    return res.data;
-  
+  const res = await API.post(`/transcript`, { input });
+  if (res.data.error == "404") return [];
+  if (res.data.error == "invalid") return "invalid";
+  return res.data;
 };
--- server/src/utils/getYtSub.ts
+++ server/src/utils/getYtSub.ts
@@ -3,18 +3,24 @@ import {
   YoutubeTranscriptNotAvailableLanguageError,
 } from "youtube-transcript";
 import { Router } from "express";
+const getYoutubeId = (input: string) =>
+  input.match(
+    /(?:youtube\.com\/(?:watch\?v=|shorts\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/,
+  )?.[1] ?? null;
+
 const router = Router();
 router.post("/api/transcript", async (req, res) => {
   const input = req.body.input;
   if (!input) return "invalid";
   try {
-    const result = await fetchTranscript(input, { lang: "ja" });
+    const id = getYoutubeId(input) ?? "";
+    const result = await fetchTranscript(id, { lang: "ja" });
+
     return res.json(result);
   } catch (err) {
     if (err instanceof YoutubeTranscriptNotAvailableLanguageError) {
       return res.json({ error: "404" });
     }
-    console.log(err)
     return res.json({ error: "invalid" });
   }
 });
