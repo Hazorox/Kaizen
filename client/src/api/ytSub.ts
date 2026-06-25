530731e0290b3b28be66b55700068d1b90d40e32
Hamza Awad <hamzaawad8899@gmail.com>
2026-06-25 06:15:08 +0300

    add 404 for when ja sub not found

--- client/src/api/ytSub.ts
+++ client/src/api/ytSub.ts
@@ -2,7 +2,8 @@ import API from "./baseAPI";
 export const getSub = async (input: string) => {
   
     const res = await API.post(`/transcript`,{input});
-    if(res.data.error) return "invalid"
+    if(res.data.error=="404") return []
+    if(res.data.error=="invalid") return "invalid"
     return res.data;
   
 };
